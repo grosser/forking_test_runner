80bc76bd0421699989aa84a063dbc4e2fd9185bf
Michael Grosser <michael@grosser.it>
2015-03-19 21:36:23 +0100

    1.9 fix

--- spec/dummy/test/test_helper.rb
+++ spec/dummy/test/test_helper.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 require_relative 'setup_test_model'
 require 'active_record/fixtures'
 
-ActiveSupport::TestCase.include ActiveRecord::TestFixtures
+ActiveSupport::TestCase.send(:include, ActiveRecord::TestFixtures)
 
 # have to tell AS where to find fixtures or it looks into the the root directory ...
 ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
