1e88da5440f20c2a8b6a07715343fa6140cfae09
Michael Grosser <michael@grosser.it>
2024-09-25 10:57:50 -0700

    give up

--- spec/dummy/test/no_ar_helper.rb
+++ spec/dummy/test/no_ar_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 # otherwise blows up in some CI combinations
-require "active_support/deprecation"
-
-require "active_support/test_case"
+require "active_support" # TODO: we only need test_case, but that breaks CI with a weird issue
 require "minitest/autorun"
 
 # TODO: find out why this needs to be here
